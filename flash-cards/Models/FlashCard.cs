82b7775ddb6ea052de74f73a3327cf02cacac317
Richard Oden <richard.thomas.oden@gmail.com>
2023-04-09 10:03:10 -0500

    Add project files.

---
+++ flash-cards/Models/FlashCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flash_cards.Models
+{
+    public class FlashCard
+    {
+        public Guid Id { get; set; }
+
+        public string Prompt { get; set; }
+
+        public string Solution { get; set; }
+
+        public int LastReportedDifficulty { get; set; }
+
+        public DateTime LastStudied { get; set; }
+
+        public FlashCard(string prompt, string solution)
+        {
+            Id = Guid.NewGuid();
+            Prompt = prompt;
+            Solution = solution;
+        }
+    }
+}
