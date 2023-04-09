9b994b34e21ad97aa79a1faac999e53d60c2d4ed
Richard Oden <richard.thomas.oden@gmail.com>
2023-04-09 10:11:22 -0500

    Added IFlashCardProvider with some basic methods.

---
+++ flash-cards/Models/IFlashCardProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flash_cards.Models
+{
+    public interface IFlashCardProvider
+    {
+        public List<FlashCard> FlashCards { get; }
+
+        public FlashCard? GetFlashCard();
+
+        public void ProcessFlashCard(FlashCard flashCard);
+
+        public void AddFlashCard(FlashCard flashCard);
+    }
+}
